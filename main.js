47f13323b0f34d088b45b964e2f4fa07f58c96a0
Amos <asidelnik@gmail.com>
2018-04-14 12:10:18 +0300

    First Secret TLV Commit

---
+++ main.js
@@ -0,0 +1,13 @@
+debugger;
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop':  $target.offset().top //no need of parseInt here
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
